ba992d6a31e2c2872e01d271f75c2ddcb9774fb5
MikhailWahib <mikhailwahib1@gmail.com>
2024-02-18 16:48:51 +0200

    Refactor scrape function

--- scrape/index.py
+++ scrape/index.py
@@ -33,76 +33,83 @@ def scrape(url):
     leagues_to_scrape = ['premier league', 'spanish la liga',
                          'german bundesliga', 'italian serie a', 'french ligue 1']
 
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    data = []
+        data = []
 
-    leagues = soup.find_all('div', {'class': leagues_class})
+        leagues = soup.find_all('div', {'class': leagues_class})
 
-    for league in leagues:
-        league_name = league.find('h3').text
+        if not leagues:
+            return []
 
-        if league_name.lower() not in leagues_to_scrape:
-            continue
+        for league in leagues:
+            league_name = league.find('h3').text
 
-        scraped_matches = []
+            if league_name.lower() not in leagues_to_scrape:
+                continue
 
-        matches = league.find_all('li', {'class': matches_class})
+            scraped_matches = []
 
-        for match in matches:
-            home_team = match.find_all('span', {'class': team_class})[0].text
-            away_team = match.find_all('span', {'class': team_class})[1].text
-            home_team_score = None
-            away_team_score = None
-            match_status = None
-            start_time = None
+            matches = league.find_all('li', {'class': matches_class})
 
-            # check if match is live
-            if match.find('span', {'class': f"{home_team_score_class} {live_score_class}"}):
-                home_team_score = match.find(
-                    'span', {'class': f"{home_team_score_class} {live_score_class}"}).text
-                away_team_score = match.find(
-                    'span', {'class': f"{away_team_score_class} {live_score_class}"}).text
-                match_status = 'playing'
-
-            # check if match is not started
-            elif match.find('span', {'class': not_started_date_class}):
+            for match in matches:
+                home_team = match.find_all(
+                    'span', {'class': team_class})[0].text
+                away_team = match.find_all(
+                    'span', {'class': team_class})[1].text
                 home_team_score = None
                 away_team_score = None
-                match_status = 'not_started'
-                start_time = match.find(
-                    'span', {'class': not_started_date_class}).text
+                match_status = None
+                start_time = None
 
-            # check if match is finished
-            elif match.find('span', {'class': f"{home_team_score_class} {finished_score_class}"}):
-                home_team_score = match.find(
-                    'span', {'class': f"{home_team_score_class} {finished_score_class}"}).text
-                away_team_score = match.find(
-                    'span', {'class': f"{away_team_score_class} {finished_score_class}"}).text
-                match_status = 'finished'
+                # check if match is live
+                if match.find('span', {'class': f"{home_team_score_class} {live_score_class}"}):
+                    home_team_score = match.find(
+                        'span', {'class': f"{home_team_score_class} {live_score_class}"}).text
+                    away_team_score = match.find(
+                        'span', {'class': f"{away_team_score_class} {live_score_class}"}).text
+                    match_status = 'playing'
 
-            scraped_matches.append({
-                'home_team': home_team,
-                'away_team': away_team,
-                # 'home_team_score': home_team_score,
-                # 'away_team_score': away_team_score,
-                'match_status': match_status,
-                # 'start_time': start_time
+                # check if match is not started
+                elif match.find('span', {'class': not_started_date_class}):
+                    home_team_score = None
+                    away_team_score = None
+                    match_status = 'not_started'
+                    start_time = match.find(
+                        'span', {'class': not_started_date_class}).text
+
+                # check if match is finished
+                elif match.find('span', {'class': f"{home_team_score_class} {finished_score_class}"}):
+                    home_team_score = match.find(
+                        'span', {'class': f"{home_team_score_class} {finished_score_class}"}).text
+                    away_team_score = match.find(
+                        'span', {'class': f"{away_team_score_class} {finished_score_class}"}).text
+                    match_status = 'finished'
+
+                scraped_matches.append({
+                    'home_team': home_team,
+                    'away_team': away_team,
+                    'match_status': match_status,
+                })
+
+                # add scores if match is finished or playing
+                if home_team_score is not None:
+                    scraped_matches[-1]['home_team_score'] = home_team_score
+                    scraped_matches[-1]['away_team_score'] = away_team_score
+
+                # add start time if match is not started
+                if start_time is not None:
+                    scraped_matches[-1]['start_time'] = start_time
+
+            data.append({
+                'title': league_name,
+                'matches': scraped_matches
             })
 
-            # add scores if match is finished or playing
-            if home_team_score is not None:
-                scraped_matches[-1]['home_team_score'] = home_team_score
-                scraped_matches[-1]['away_team_score'] = away_team_score
+        return data
 
-            # add start time if match is not started
-            if start_time is not None:
-                scraped_matches[-1]['start_time'] = start_time
-
-        data.append({
-            'title': league_name,
-            'matches': scraped_matches
-        })
-
-    return data
+    except:
+        print('Something went wrong while scraping the data')
+        return None
