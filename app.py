1943fa72976d177ba4d4f5c918d381c5ed8b8450
MikhailWahib <mikhailwahib1@gmail.com>
2024-02-18 16:49:19 +0200

    Add /<date> endpoint

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from scrape.index import scrape
+import re
+from werkzeug.exceptions import BadRequest
 
 app = Flask(__name__)
 app.json.sort_keys = False
@@ -12,9 +14,49 @@ def hello():
     })
 
 
+def handle_error_response(data):
+    if data is None:
+        return jsonify({
+            "error": "Something went wrong"
+        }), 500
+
+
+'''
+if no data it returns an empty list
+if there is an error it returns an error message with a 500 status code
+'''
+
+
 @app.route('/today')
 def index():
     data = scrape('https://www.bbc.com/sport/football/scores-fixtures')
+
+    error_message = handle_error_response(data)
+
+    if error_message:
+        return error_message
+
+    return jsonify({
+        "leagues": data
+    })
+
+
+# /mm-dd
+@app.route('/<date>')
+def date(date):
+    if not re.match(r'^\d{2}-\d{2}$', date):
+        return jsonify({
+            "error": "Invalid date format, should be mm-dd"
+        }), 400
+
+    data = scrape(
+        f'https://www.bbc.com/sport/football/scores-fixtures/2024-{date}')
+
+    error_message = handle_error_response(data)
+
+    if error_message:
+        return error_message
+
     return jsonify({
         "leagues": data
     })
